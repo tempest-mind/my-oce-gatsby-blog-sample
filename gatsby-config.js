7733dd1a3fea45d2f4f6f9c25a0912ff5177cd08
CJ <cj.pintacura@oracle.com>
2022-05-24 14:56:38 -0700

    Adding support for INCOMING_HOOK_BODY

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,25 @@
 
 require('dotenv').config();
 
+let serverUrl;
+let channelToken;
+let proxyUrl;
+console.log('process.env.INCOMING_HOOK_BODY', process.env.INCOMING_HOOK_BODY);
+let incomingHookBody = decodeURIComponent(process.env.INCOMING_HOOK_BODY || '');
+if (incomingHookBody) {
+  try {
+    incomingHookBody = JSON.parse(incomingHookBody);
+    serverUrl = incomingHookBody.SERVER_URL;
+    channelToken = incomingHookBody.CHANNEL_TOKEN;
+    proxyUrl = incomingHookBody.PROXY_URL;
+    console.log('INCOMING_HOOK_BODY.SERVER_URL', serverUrl);
+    console.log('INCOMING_HOOK_BODY.CHANNEL_TOKEN', channelToken);
+    console.log('INCOMING_HOOK_BODY.PROXY_URL', proxyUrl);
+  } catch (e) {
+    console.error('Error parsing INCOMING_HOOK_BODY', incomingHookBody);
+  }
+}
+
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX,
   siteMetadata: {
@@ -24,9 +43,9 @@ module.exports = {
       resolve: '@oracle/gatsby-source-oce',
       options: {
         name: 'oce',
-        contentServer: process.env.SERVER_URL,
-        channelToken: process.env.CHANNEL_TOKEN,
-        proxyUrl: process.env.PROXY_URL,
+        contentServer: serverUrl || process.env.SERVER_URL,
+        channelToken: channelToken || process.env.CHANNEL_TOKEN,
+        proxyUrl: proxyUrl || process.env.PROXY_URL,
         items: {
           limit: 4,
           query: '',
